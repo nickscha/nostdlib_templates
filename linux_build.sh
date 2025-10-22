91077d599dde88e21f22406b7ccfca33b90ce511
nickscha <nick@scharnbeck.de>
2025-10-22 18:27:28 +0200

    [LINUX] added hello world nostdlib

---
+++ linux_build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+# -----------------------------------------------------------------------------
+# Compiler and linker configuration (Linux nostdlib build)
+# -----------------------------------------------------------------------------
+DEF_COMPILER_FLAGS="-march=native -mtune=native -std=c89 -pedantic \
+-nodefaultlibs -nostdlib \
+-fno-builtin -ffreestanding -fno-asynchronous-unwind-tables \
+-fomit-frame-pointer -fuse-ld=lld \
+-Wall -Wextra -Werror -Wvla -Wconversion -Wdouble-promotion \
+-Wsign-conversion -Wmissing-field-initializers -Wuninitialized -Winit-self \
+-Wunused -Wunused-macros -Wunused-local-typedefs"
+
+DEF_LINKER_FLAGS="-static -s"
+
+# -----------------------------------------------------------------------------
+# Entry point function for nostdlib binaries
+# -----------------------------------------------------------------------------
+ENTRY_POINT="-Wl,-e,nostdlib_main"
+
+# -----------------------------------------------------------------------------
+# Build Targets
+# -----------------------------------------------------------------------------
+cc -O2 $DEF_COMPILER_FLAGS $ENTRY_POINT linux_hello_world.c -o linux_hello_world $DEF_LINKER_FLAGS
+
+# -----------------------------------------------------------------------------
+# Run tests
+# -----------------------------------------------------------------------------
+./linux_hello_world
---
+++ linux_hello_world.c
@@ -0,0 +1,200 @@
+/* nostdlib_templates - v0.2 - public domain data structures - nickscha 2025
+
+C89 standard compliant, nostdlib/nolibc templates (NOSTDLIB_TEMPLATES).
+
+LICENSE
+
+  Placed in the public domain and also MIT licensed.
+  See end of file for detailed license information.
+
+*/
+
+/* ############################################################################
+ * # COMPILER Settings
+ * ############################################################################
+ *
+ * Some versions of GCC/Clang still tries to call memset/memcpy even with
+ * having "-fno-builtin" set.
+ */
+#ifdef _MSC_VER
+#pragma function(memset)
+#endif
+void *memset(void *dest, int c, unsigned int count)
+{
+    char *bytes = (char *)dest;
+    while (count--)
+    {
+        *bytes++ = (char)c;
+    }
+    return dest;
+}
+
+#ifdef _MSC_VER
+#pragma function(memcpy)
+#endif
+void *memcpy(void *dest, const void *src, unsigned int count)
+{
+    char *dest8 = (char *)dest;
+    const char *src8 = (const char *)src;
+    while (count--)
+    {
+        *dest8++ = *src8++;
+    }
+    return dest;
+}
+
+/* ############################################################################
+ * # Minimal Linux syscall interface
+ * ############################################################################
+ *
+ * Avoiding glibc: we call the kernel directly via syscall numbers.
+ * Works on x86_64 and i386 (adjust as needed).
+ */
+#if defined(__x86_64__)
+
+static inline long sys_write(unsigned int fd, const char *buf, unsigned long len)
+{
+    long ret;
+    __asm__ volatile(
+        "syscall"
+        : "=a"(ret)
+        : "a"(1), "D"(fd), "S"(buf), "d"(len)
+        : "rcx", "r11", "memory");
+    return ret;
+}
+
+static inline void sys_exit(int code)
+{
+    __asm__ volatile(
+        "movq $60, %%rax\n\t"
+        "movq %0, %%rdi\n\t"
+        "syscall"
+        :
+        : "r"((long)code)
+        : "rax", "rdi");
+    for (;;)
+        ;
+}
+
+#elif defined(__i386__)
+
+static inline long sys_write(unsigned int fd, const char *buf, unsigned long len)
+{
+    long ret;
+    __asm__ volatile(
+        "int $0x80"
+        : "=a"(ret)
+        : "a"(4), "b"(fd), "c"(buf), "d"(len)
+        : "memory");
+    return ret;
+}
+
+static inline void sys_exit(int code)
+{
+    __asm__ volatile(
+        "int $0x80"
+        :
+        : "a"(1), "b"(code)
+        : "memory");
+    for (;;)
+        ;
+}
+
+#elif defined(__aarch64__)
+
+static inline long sys_write(unsigned int fd, const char *buf, unsigned long len)
+{
+    register long x8 __asm__("x8") = 64; /* syscall number: write */
+    register long x0 __asm__("x0") = fd;
+    register const char *x1 __asm__("x1") = buf;
+    register long x2 __asm__("x2") = len;
+    __asm__ volatile(
+        "svc 0"
+        : "+r"(x0)
+        : "r"(x1), "r"(x2), "r"(x8)
+        : "memory");
+    return x0;
+}
+
+static inline void sys_exit(int code)
+{
+    register long x8 __asm__("x8") = 93; /* syscall number: exit */
+    register long x0 __asm__("x0") = code;
+    __asm__ volatile(
+        "svc 0"
+        :
+        : "r"(x0), "r"(x8)
+        : "memory");
+    for (;;)
+        ;
+}
+
+#else
+#error "Unsupported architecture: x86_64, i386, or aarch64 only"
+#endif
+
+/* ############################################################################
+ * # Main entry point
+ * ############################################################################
+ */
+#ifdef __clang__
+#elif __GNUC__
+__attribute((externally_visible))
+#endif
+#ifdef __i686__
+__attribute((force_align_arg_pointer))
+#endif
+int nostdlib_main(void)
+{
+    const char msg[] = "Hello World!\n";
+
+    /* Write to STDOUT (fd = 1) using syscall */
+    sys_write(1, msg, (unsigned long)(sizeof(msg) - 1));
+
+    /* Exit cleanly with code 0 */
+    sys_exit(0);
+
+    return 0; /* not reached */
+}
+
+/*
+   ------------------------------------------------------------------------------
+   This software is available under 2 licenses -- choose whichever you prefer.
+   ------------------------------------------------------------------------------
+   ALTERNATIVE A - MIT License
+   Copyright (c) 2025 nickscha
+   Permission is hereby granted, free of charge, to any person obtaining a copy of
+   this software and associated documentation files (the "Software"), to deal in
+   the Software without restriction, including without limitation the rights to
+   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+   of the Software, and to permit persons to whom the Software is furnished to do
+   so, subject to the following conditions:
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+   ------------------------------------------------------------------------------
+   ALTERNATIVE B - Public Domain (www.unlicense.org)
+   This is free and unencumbered software released into the public domain.
+   Anyone is free to copy, modify, publish, use, compile, sell, or distribute this
+   software, either in source code form or as a compiled binary, for any purpose,
+   commercial or non-commercial, and by any means.
+   In jurisdictions that recognize copyright laws, the author or authors of this
+   software dedicate any and all copyright interest in the software to the public
+   domain. We make this dedication for the benefit of the public at large and to
+   the detriment of our heirs and successors. We intend this dedication to be an
+   overt act of relinquishment in perpetuity of all present and future rights to
+   this software under copyright law.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+   WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+   ------------------------------------------------------------------------------
+*/
