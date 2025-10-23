2e3e1abb64c09a3a4b0d4b813da578e4be667272
nickscha <nick@scharnbeck.de>
2025-10-23 20:19:28 +0200

    [DOC] windows build bat remarks

--- win32_build.bat
+++ win32_build.bat
@@ -4,6 +4,9 @@ REM "-mwindows" - Use when you want a window application without console
 REM "-Xlinker /ENTRY:nostdlib_main" - What is the entry point function name
 REM "-Xlinker /STACK:0x100000,0x100000" - This sets the stack size (1 MB)
 
+REM ---------------------------------------------------------------------------
+REM Compiler and Linker Configuration
+REM ---------------------------------------------------------------------------
 set DEF_COMPILER_FLAGS=-mconsole -march=native -mtune=native -std=c89 -pedantic -nodefaultlibs -nostdlib -mno-stack-arg-probe ^
 -Xlinker /STACK:0x100000,0x100000 ^
 -Xlinker /ENTRY:nostdlib_main ^
@@ -12,9 +15,15 @@ set DEF_COMPILER_FLAGS=-mconsole -march=native -mtune=native -std=c89 -pedantic 
 
 set DEF_FLAGS_LINKER=-lkernel32
 
+REM ---------------------------------------------------------------------------
+REM Build Targets
+REM ---------------------------------------------------------------------------
 cc -s -O2 %DEF_COMPILER_FLAGS% win32_hello_world.c -o win32_hello_world.exe %DEF_FLAGS_LINKER%
 cc -s -O2 %DEF_COMPILER_FLAGS% win32_hello_cli.c -o win32_hello_cli.exe %DEF_FLAGS_LINKER%
 cc -s -O2 -shared %DEF_COMPILER_FLAGS% win32_hello_dll.c -o win32_hello_dll.dll %DEF_FLAGS_LINKER%
 
+REM ---------------------------------------------------------------------------
+REM Run tests
+REM ---------------------------------------------------------------------------
 win32_hello_world.exe
 win32_hello_cli.exe test_argument foo bar
